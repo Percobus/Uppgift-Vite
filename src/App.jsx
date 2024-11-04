fe6f997de40eb067785be8c046bebfc56390df71
Filip Persson <filip@percobus.se>
2024-11-04 16:34:45 +0100

    Test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Header from './components/Header'
-import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import Testimonials from './components/Testimonials'
 import Faq from './components/Faq'
 import Subscribe from './components/Subscribe'
 import Brands from './components/Brands'
+import Footer from './components/Footer'
 
 function App() {
 
